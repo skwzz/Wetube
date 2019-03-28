b514b2e345311c091058662800aa9df9fca9fff1
skwzz <kidol9108@gmail.com>
2019-03-28 18:01:45 +0900

    defined models

--- db.js
+++ db.js
@@ -1,41 +1,15 @@
-export const dummy = [
-  {
-    id: 1341,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 11111,
-      name: "SKW",
-      email: "SKW@grrrr.com"
-    }
-  },
-  {
-    id: 2222,
-    title: "Video Nice",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 11111,
-      name: "SKW",
-      email: "SKW@grrrr.com"
-    }
-  },
-  {
-    id: 3333,
-    title: "Video perfect",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 11111,
-      name: "SKW",
-      email: "SKW@grrrr.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = error => console.log(`Error on DB connection:${error}`);
+db.once("open", handleOpen);
+db.on("error", handleError);
--- init.js
+++ init.js
@@ -1,12 +1,13 @@
 import app from "./app";
-import testApp from "./testApp";
-const PORT = 4040;
-const PORT2 = 4041;
+import "./db";
+import dotenv from "dotenv";
+dotenv.config();
+import "./models/Video";
+import "./models/Comment";
+
+const PORT = process.env.PORT || 4040;
+
 const handleListening = () =>
   console.log(`Listening on : http://localhost:${PORT}`);
 
-const handleListening2 = () =>
-  console.log(`Listening on : http://localhost:${PORT2}`);
-
 app.listen(PORT, handleListening);
-//testApp.listen(PORT2, handleListening2);
